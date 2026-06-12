490176e51bdc5b7e41500938fc8c011d6681142e
uriino23 <umendozaino@gmail.com>
2026-06-12 09:10:49 -0600

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,7 @@
+n = int(input("Introduce el primer número entero (n): "))
+m = int(input("Introduce el segundo número entero (m): "))
+
+c = n // m
+r = n % m
+
+print(f"{n} entre {m} da un cociente {c} y un residuo {r}")
